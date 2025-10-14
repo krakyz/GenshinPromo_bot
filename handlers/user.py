9b36af85837d6820e72c18affb0b379b08ea4ea2
Stepan Surkov <svs441@yandex.ru>
2025-10-14 05:36:13 +0500

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -95,9 +95,9 @@ class MessageTemplates:
         for code in codes:
             text += f"<code>{code.code}</code>\n"
             text += f"<i>{code.description or 'MISSING_CODE'}</i>\n"
-            text += f"<i>{code.rewards or 'Не указано'}</i>\n\n"
+            text += f"<i>{code.rewards or 'Не указано'}</i>\n"
             if code.expires_date:
-                text += f"⏰ До: {format_expiry_date(code.expires_date)}\n\n"
+                text += f"⏰ Активен до {format_expiry_date(code.expires_date)}\n\n"
             
         return text
     
